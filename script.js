bcc83a08c35395027d0612cdee8ead4f7d019310
Jesse112987 <115638162+Jesse112987@users.noreply.github.com>
2023-05-25 21:02:21 +0000

    De ui en allemaal menu's gemaakt voor in de game

--- script.js
+++ script.js
@@ -14,9 +14,16 @@
 /* ********************************************* */
 /* globale variabelen die je gebruikt in je game */
 /* ********************************************* */
+const MAINMENU = 0;
 const SPELEN = 1;
 const GAMEOVER = 2;
-var spelStatus = SPELEN;
+const UITLEGSCHERM = 3;
+const UITLEGSCHERM2 = 4;
+const LEVELSCHERM = 5;
+const QUITSCHERM = 6;
+const RETRYSCHERM = 7;
+const PAUZEERSCHERM = 8;
+var spelStatus = MAINMENU;
 
 // mogelijkheden voor de richting van vijandjes en speler
 const LEFT = 0;
@@ -70,8 +77,17 @@ var img12;
 var img13;
 var img14;
 var img15;
+var img16;
+var img17;
+var img18;
+var img19;
+var img20;
+var img21;
+var img22;
+var img23;
+var img24;
 
-//tijd
+// tijd in de game
 var tijd;
 var tijdOver = 6000;
 
@@ -79,6 +95,161 @@ var tijdOver = 6000;
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
 /* ********************************************* */
+
+
+/**
+ * Main menu voor de game wordt hier gemaakt
+*/
+var mainMenu = function () {
+// achtergrond
+image(img16, 0, 0, 1280, 720);
+
+// buttons
+image(img14, 565, 400, 150, 150);  // play button
+image(img11, 470, 390, 70, 70);    // info button
+image(img17, 740, 390, 70, 70);    // level button
+
+  // Button voor om te spelen (midden)
+  if(mouseX > 565 && mouseX < 715 && mouseY > 400 && mouseY < 550){
+      if(mouseIsPressed === true){
+          spelStatus = SPELEN;
+          beginTijd();
+        }
+  }
+
+  // Button voor naar de uitlegscherm (linker)
+  if(mouseX > 470 && mouseX < 540 && mouseY > 390 && mouseY < 460){
+      if(mouseIsPressed === true){
+          spelStatus = UITLEGSCHERM;
+        }
+  }
+
+  // Button voor om naar de levels te gaan (rechter)
+  if(mouseX > 740 && mouseX < 810 && mouseY > 390 && mouseY < 460){
+      if(mouseIsPressed === true){
+          spelStatus = LEVELSCHERM;
+        }
+  }
+
+  image(img18, 280, 180);
+  
+};
+
+
+/** 
+ * Uitlegscherm in de main menu van de game wordt hier gemaakt
+*/
+var uitlegScherm = function () {
+  // achtergrond
+image(img16, 0, 0, 1280, 720);
+
+  // button om terug te gaan naar de main menu
+image(img19, 40, 600, 100, 100);
+
+  if(mouseX > 40 && mouseX < 140 && mouseY > 600 && mouseY < 700){
+      if(mouseIsPressed === true){
+          spelStatus = MAINMENU;
+        }
+  }
+  
+};
+
+
+/** 
+ * Buttons tijdens het spelen van game
+*/
+
+// Uitlegscherm tijdens het spelen van de game wordt hier gemaakt
+var uitlegScherm2 = function() {
+  //achtergrond
+  image(img16, 0, 0, 1280, 720);
+
+  // button om terug te gaan naar het spel
+  image(img19, 50, 600, 100, 100);
+
+  if(mouseX > 50 && mouseX < 150 && mouseY > 600 && mouseY < 700){
+      if(mouseIsPressed === true){
+          spelStatus = SPELEN;
+        }
+  }
+};
+
+/** 
+ * Level scherm voor de game
+*/
+var levelScherm = function() {
+  // achtergrond
+  image(img16, 0, 0, 1280, 720);
+
+  // button om terug te gaan naar main menu
+  image(img19, 40, 600, 100, 100);
+
+  if(mouseX > 40 && mouseX < 140 && mouseY > 600 && mouseY < 700){
+      if(mouseIsPressed === true){
+          spelStatus = MAINMENU;
+        }
+  }
+};
+
+/** 
+ * Retry scherm om de game opnieuw te beginnen
+*/
+var retryScherm = function() {
+  fill('black')
+  rect(200, 200, 880, 380);
+
+  textSize(40);
+  fill("white");
+  text("Weet je zeker dat je wil restarten?", 250, 250);
+
+  image(img22, 440, 300, 100, 100); // Ja button
+  image(img15, 840, 300, 100, 100); // Nee button
+
+
+  // Code om de game te restarten
+  if(mouseX > 440 && mouseX < 540 && mouseY > 300 && mouseY < 400){
+      if(mouseIsPressed === true){
+          spelStatus = SPELEN;
+          resetGlobalVariables();
+        }
+}
+
+  // Code voor de nee button 
+  if(mouseX > 840 && mouseX < 940 && mouseY > 300 && mouseY < 400){
+      if(mouseIsPressed === true){
+          spelStatus = SPELEN;
+        }
+}
+  
+};
+
+/** 
+ * Pauzeer de game tijdens het spelen
+*/
+var quitScherm = function() {
+  //achtergrond
+  image(img16, 100, 100, 1080, 520);
+
+  textSize();
+  fill("white");
+  text("Weet je zeker dat je wil stoppen?", 100, 100);
+  rect(200, 175, 30, 30);
+  text("JA", 200, 200);
+  rect(200, 275, 60, 30);
+  text("NEE", 200, 300);
+
+  if(mouseX > 200 && mouseX < 230 && mouseY > 175 && mouseY < 205){
+      if(mouseIsPressed === true){
+          spelStatus = MAINMENU;
+          resetGlobalVariables();
+        }
+  }
+  if(mouseX > 200 && mouseX < 260 && mouseY > 275 && mouseY < 305){
+      if(mouseIsPressed === true){
+          spelStatus = SPELEN;
+        }
+  }
+}
 
 /**
  * Updatet globale variabelen met posities van speler, vijanden en kogels
@@ -162,16 +333,15 @@ var verwerkBotsing = function() {
    muntjes = muntjes + 1;
    vakjes[j][i] = 0;
  }
-  // botsing kogel tegen vijand
-
+  
 };
 
 /**
  * Tekent spelscherm
  */
 var tekenAlles = function() {
-  // speelveld
   
+  // speelveld
   image(img1, 0, 0, 1280, 720);
   
   
@@ -201,14 +371,7 @@ var tekenAlles = function() {
     }
     }
   }
-
-  // vijand
-
-  // kogel
-
-  // speler
-
-
+// speler richtingen tijdens het lopen
   var imageToUse = img2;
   
 if (spelerRichting === RIGHT && spelerBeweging === false) {
@@ -231,9 +394,7 @@ imageToUse = img7;
 spelerGrootteX = 100;
 }
   
-  // punten en health
-  
-  // scoreboard
+  // scoreboard tijdens het spelen
   noSmooth()
   image(imageToUse, spelerX, spelerY, spelerGrootteX, 75);
   image(img4, 1020, 0, 1200, 720)
@@ -265,37 +426,50 @@ spelerGrootteX = 100;
   fill(0,0,0);
   var label = spelerX + " , " + spelerY;
   text(label, spelerX + 40, spelerY + 20);
-  
-};
 
-// menu buttons
-
-// info button
-/* 
-if(mouseIsPressed && mouseX > 1120 && mouseX < 1190 && mouseY > 330 && mouseY < 400){
-  fill("black");
-
-}
+/**  
+ * Menu buttons in game 
 */
 
+  // Info button
+if(mouseX > 1120 && mouseX < 1190 && mouseY > 330 && mouseY < 400){
+  if(mouseIsPressed === true){
+          spelStatus = UITLEGSCHERM2;
+        }
+  } 
 
+  // Quit button
+if(mouseX > 1120 && mouseX < 1190 && mouseY > 630 && mouseY < 700){
+  if(mouseIsPressed === true){
+          spelStatus = QUITSCHERM;
+        }
+  }
+
+  // Retry button
+  if(mouseX > 1120 && mouseX < 1190 && mouseY > 430 && mouseY < 500){
+  if(mouseIsPressed === true){
+          spelStatus = RETRYSCHERM;
+        }
+  }
+
+  
+};
 
 //tijd in game over
 function beginTijd () {
   tijd = setInterval(updateTijd, 1000);
-}
-beginTijd();
+};
 updateTijd();
 function updateTijd() {
   tijdOver = tijdOver - 1;
-}
+};
 
 /**
  * return true als het gameover is
  * anders return false
  */
 var checkGameOver = function() {
-  // check of HP 0 is , of tijd op is, of muntjes allemaal gepakt zijn
+  // check of HP 0 is , of tijd op is of muntjes allemaal gepakt zijn
   if(health === 0){
     return true;
   }
@@ -307,6 +481,30 @@ var checkGameOver = function() {
   }
   return false;
 };
+
+/** 
+ * Restart de game en reset alle variables naar de standaard waarden om spel opnieuw te 
+ * spelen
+*/
+function resetGlobalVariables(){
+  muntjes = 0;
+  health = 3;
+  tijdOver = 60;
+  spelerRichting = RIGHT;
+  spelerBeweging = false;
+  spelerX = 500; 
+  spelerY = 355;
+  vakjes = [   [ 1,2,1,0,1,0,0,0,0,3,3,1], 
+               [ 1,3,0,3,3,1,1,0,0,2,0,3],       
+               [ 2,1,1,0,0,0,0,0,0,1,3,0],
+               [ 0,0,0,0,3,0,0,1,0,0,0,0],
+               [ 3,1,1,0,0,0,0,0,0,3,3,0],
+               [ 0,1,0,3,3,0,3,0,0,3,0,1],
+               [ 3,1,1,0,1,3,0,3,0,0,1,2],
+               [ 0,2,1,1,3,3,1,0,3,0,3,0]
+               ];
+}
+
 
 /* ********************************************* */
 /* setup() en draw() functies / hoofdprogramma   */
@@ -332,6 +530,13 @@ function preload() {
   img13 = loadImage('afbeeldingen/pausebutton.png');
   img14 = loadImage('afbeeldingen/playbutton.png');
   img15 = loadImage('afbeeldingen/quitbutton.png');
+  img16 = loadImage('afbeeldingen/mainmenu.png');
+  img17 = loadImage('afbeeldingen/levelsbutton.png');
+  img18 = loadImage('afbeeldingen/textmainmenu.png');
+  img19 = loadImage('afbeeldingen/returnbutton.png');
+  img20 = loadImage('afbeeldingen/idlecharacterzwartenwit.gif');
+  img21 = loadImage('afbeeldingen/idlecharacter2zwartenwit.gif');
+  img22 = loadImage('afbeeldingen/markbutton.png');
 }
 
 /**
@@ -350,6 +555,24 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+  if (spelStatus === MAINMENU) {
+    mainMenu();
+  }
+  if (spelStatus === UITLEGSCHERM) {
+    uitlegScherm();
+  }
+  if (spelStatus === UITLEGSCHERM2) {
+    uitlegScherm2();
+  }
+  if (spelStatus === LEVELSCHERM){
+    levelScherm();
+  }
+  if (spelStatus === QUITSCHERM){
+    quitScherm();
+  }
+  if (spelStatus === RETRYSCHERM){
+    retryScherm();
+  }
   if (spelStatus === SPELEN) {
     beweegAlles();
     verwerkBotsing();
@@ -364,50 +587,68 @@ function draw() {
     rect(0,0,10000, 2000);
     fill(255,255,255);
     textSize(200);
-    text("GAMEOVER", 90, 200);
-    
-    textSize(100);
-    fill("lime");
-    text("Retry", 190, 500);
-    fill("red");
-    text("Quit", 950, 500);
+    text("GAMEOVER", 80, 250);
 
-  textSize(40);
+    image(img12, 340, 420, 170, 170);   // retry button
+    image(img15, 770, 420, 170, 170);   // terug button
+    image(img20, 90, 300);              // plaatje links
+    image(img21, 980, 300);             // plaatje rechts
+
+  // score
+  textSize(60);
   if(muntjes === 5 && tijd > 0){
             fill(255,255,255);
-            text("Score:", 600, 400);
+            text("Score:", 520, 400);
             fill(15, 173, 12);
-            text("A", 725, 400);
+            text("A", 715, 400);
 }
   if(muntjes === 4 && tijd > 0){
             fill(255,255,255);
-            text("Score:", 600, 400);
+            text("Score:", 520, 400);
             fill(219, 74, 48);
-            text("B", 725, 400);
+            text("B", 715, 400);
   }
     if(muntjes === 3 && tijd >= 0){
             fill(255,255,255);
-            text("Score:", 600, 400);
+            text("Score:", 520, 400);
             fill(230, 130, 30);
-            text("C", 725, 400);
+            text("C", 715, 400);
     }
       if(muntjes === 2 && tijd >= 0){
             fill(255,255,255);
-            text("Score:", 600, 400);
+            text("Score:", 520, 400);
             fill(69, 105, 68);
-            text("D", 725, 400);
+            text("D", 715, 400);
       }
         if(muntjes === 1 && tijd >= 0){
             fill(255,255,255);
-            text("Score:", 600, 400);
+            text("Score:", 520, 400);
             fill(2, 34, 97);
-            text("E", 725, 400);
+            text("E", 715, 400);
           }
           if(muntjes === 0 && tijd >= 0){
             fill(255,255,255);
-            text("Score:", 600, 400);
+            text("Score:", 520, 400);
             fill(102, 78, 7);
-            text("F", 725, 400);
+            text("F", 715, 400);
           }
+
+    // retry button
+    if(mouseX > 340 && mouseX < 510 && mouseY > 420 && mouseY < 590){
+  if(mouseIsPressed === true){
+          spelStatus = SPELEN;
+          resetGlobalVariables();
+        }
   }
-}
+
+    // terug button
+    if(mouseX > 770 && mouseX < 940 && mouseY > 420 && mouseY < 590){
+  if(mouseIsPressed === true){
+          spelStatus = MAINMENU;
+          resetGlobalVariables();
+        }
+  }
+
+    
+  }
+};
